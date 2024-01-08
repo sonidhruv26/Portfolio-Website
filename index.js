33af248a8114edadce6d560a2fbd464337e7faf4
sonidhruv26 <sonidhruv.2603@gmail.com>
2024-01-08 14:02:27 +0530

    Update server port configuration

--- index.js
+++ index.js
@@ -62,4 +62,8 @@ app.post('/submit-form', (req, res) => {
         })
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
